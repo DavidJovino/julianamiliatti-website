df1c5ce1cdd95e8a2aadee090d72144186216b2a
DavidJovino <davidjovino@live.com>
2025-12-08 23:56:24 -0300

    Add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/julianamiliatti-website/',
   plugins: [react()],
   resolve: {
     alias: {
